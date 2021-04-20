5f6e989ff28d229e8e9c0e6d57e25d27f0fa3f38
Yosbel Martinez <yosbel.martinez2@gcelsa.com>
2021-04-20 13:07:42 +0200

    Added condition checking for the where condition of the query

--- builders.go
+++ builders.go
@@ -2,11 +2,15 @@ package querybuilder
 
 import "fmt"
 
-func SelectColumnsFromTableWhereCondition(columns []Column, tableName string ,condition Condition, options TableOptions) string {
+func SelectColumnsFromTableWhereCondition(columns []Column, tableName string, condition Condition, options TableOptions) string {
 	cols := parseColumns(columns, options)
 	cond := parseCondition(condition)
+	whereCondition := ""
+	if cond != "" {
+		whereCondition = fmt.Sprintf("WHERE %s", cond)
+	}
 
-	return fmt.Sprintf("SELECT %s FROM %s WHERE %s", cols, tableName, cond)
+	return fmt.Sprintf("SELECT %s FROM %s %s", cols, tableName, whereCondition)
 }
 
 func UpdateTableSetColumnWherePrimaryKeyInIds(tableName string, updateStatement Condition, key string, ids string) string {
